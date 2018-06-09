bc59ce972f91abdf2bfc864fa025ebb4e57293de
Mark Sorce <msorce@users.noreply.github.com>
2018-06-09 11:04:46 -0700

    Update input.sh

--- input.sh
+++ input.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
-defaults write -g InitialKeyRepeat -int 10
+# install homebrew package manager
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# install neovim
+brew install neovim
+
+# symlink vim -> nvim configs
+ln -s ~/.vim ~/.config/nvim
+ln -s ~/.vimrc ~/.config/nvim/init.vim
+
+# zsh > bash. jk, who cares? you shouldn't :)
+brew install zsh
+
+# install iterm 2
+brew cask install iterm2
+
+# fast key repeats
+defaults write -g InitialKeyRepeat -int 5
 defaults write -g KeyRepeat -int 1
